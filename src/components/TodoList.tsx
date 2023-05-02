1564ec6ce47177a71eea53dee19e5382e5e33b04
clayton-klein <csklein87@hotmail.com>
2023-05-02 15:29:01 -0300

    feat: add delete functionality.

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,20 +1,31 @@
 import "../styles/TodoList.css";
-// the type of the "props" we want to receive
+
+// the type of the "props" we want to receive.
 interface TodoListProps {
   items: { id: string; text: string }[];
+  onDeleteTodo: (id: string) => void;
 }
 
 // attributing the created type to the props.
-/**
- * The JSX.Element type is implicitly returned by TS, so there's
- * no need to add it, here we're just being explicit for the sake
- * of exercising.
- */
 export default function TodoList(props: TodoListProps): JSX.Element {
   return (
     <ul>
       {props.items.map((todo) => (
-        <li key={todo.id}>{todo.text}</li>
+        <li key={todo.id}>
+          <span>{todo.text}</span>
+          {/**
+           * We expect to receive the "todo id" as a parameter of
+           * this handler, so we had to "bind" it, the first
+           * argument is about the "this" keyword, we don't care
+           * about it in this case so we set it to "null" and the
+           * second argument will be the first argument of the
+           * "onDeleteTodo" handler. Without binding, it wouldn't
+           * work.
+           */}
+          <button onClick={props.onDeleteTodo.bind(null, todo.id)}>
+            DELETE
+          </button>
+        </li>
       ))}
     </ul>
   );

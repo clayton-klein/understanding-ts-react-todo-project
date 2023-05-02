9f57fc8f1436e062f5f6386f5d9e57d79ebca79a
clayton-klein <csklein87@hotmail.com>
2023-05-02 15:32:40 -0300

    feat: set state & add & delete todo's handlers.

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,33 @@
+import { useState } from "react";
 import TodoList from "./components/TodoList";
 import NewTodo from "./components/NewTodo";
+import { Todo } from "./todo.model";
 
+/**
+ * The JSX.Element type is implicitly returned by TS, so there's
+ * no need to add it, here we're just being explicit for the sake
+ * of exercising.
+ */
 export default function App(): JSX.Element {
-  const todos = [{ id: "td1", text: "finish course" }];
+  const [todos, setTodos] = useState<Todo[]>([]);
 
   function todoAddHandler(text: string) {
-    console.log(text);    
+    setTodos((prevTodos) => [
+      ...prevTodos,
+      { id: Math.random().toString(), text: text },
+    ]);
+  }
+
+  function todoDeleteHandler(todoId: string) {
+    setTodos((prevTodos) => {
+      return prevTodos.filter(todo => todo.id !== todoId);
+    });
   }
 
   return (
     <>
       <NewTodo onAddTodo={todoAddHandler} />
-      <TodoList items={todos} />
+      <TodoList items={todos} onDeleteTodo={todoDeleteHandler} />
     </>
   );
 }
